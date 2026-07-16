26fc24b3ba841285af1c6d3f53c478a76a1909a2
hiteke <hiteke@mail.ru>
2026-07-16 21:00:39 +0300

    HW9

--- src/main/java/Homework9/Task2/Triangle.java
+++ src/main/java/Homework9/Task2/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle extends Figure {
     public double sideB;
     public double sideC;
 
-    public Triangle(double side1, double side2, double side3) {
+    public Triangle(double sideA, double sideB, double sideC) {
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
